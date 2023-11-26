75e24424e9c9b96ba9b52bbc700c17afa08bfa0a
anguzmanc <82405873+anguzmanc@users.noreply.github.com>
2023-11-26 16:55:10 -0500

    Update automatico.v

--- Prácticas/Proyecto/Compuerta/Source/automatico.v
+++ Prácticas/Proyecto/Compuerta/Source/automatico.v
@@ -7,13 +7,12 @@ reg [10:0] count_espera = 0;
 reg [3:0] count = 0;
 
 always @(posedge clk_in) begin
-//500
-	if (count_espera < 800) begin
+	if (count_espera < 800) begin //26 segundos
 		count_espera = count_espera+1;
 	end
 	else begin
 		count_espera=0;
-		if(count<10) begin
+		if(count<10) begin //3 segundos
 			count = count+1;
 			mover_servo =1;
 		end
